6fd6b39e17dac7eb232b8b0d7f7327fefa1d98b0
unknown <shalom2147@gmail.com>
2023-07-27 10:55:31 +0300

    GUI features added

--- dachat.py
+++ dachat.py
@@ -10,18 +10,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from fpdf import FPDF
 import os.path
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter import messagebox
 
 os.sys.path.insert(0, os.path.abspath('..'))
 
-def file_verifier(F):
-    F = str(input('What is the file name?\n'))
-    while True:
-        if not os.path.exists(F):
-            print('Invalid path, try again\n')
-            continue
-        break
-    return F
-def rawtodf(file, key, category):
+
+def rawtodf(file, category):
     
     split_formats = {
         '12hr' : '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APap][mM]\s-\s',
@@ -33,7 +30,16 @@ def rawtodf(file, key, category):
         '24hr' : '%d/%m/%Y, %H:%M - ',
         'custom': ''
     }
-    
+    def hour_format(file_path):
+        raw = open(file_path, 'r', encoding='utf-8')
+        content = raw.read()
+        if bool(re.search(split_formats.get('12hr'), content)):
+            return '12hr'
+        elif bool(re.search(split_formats.get('24hr'), content)):
+            return '24hr'
+        else:
+            tk.messagebox.showwarning(title='Invalid Input', message='txt file hour format doesnt match!')
+    key = hour_format(file)
     with open(file, 'r', encoding='utf-8') as raw_data:
         raw_string = ' '.join(raw_data.read().split('\n')) # converting the list split by newline char. as one whole string as there can be multi-line messages
         user_msg = re.split(split_formats[key], raw_string) [1:] # splits at all the date-time pattern, resulting in list of all the messages with user names
@@ -73,35 +79,14 @@ def rawtodf(file, key, category):
     df.drop('user_msg', axis=1, inplace=True)
     
     return df
-def hour_formt_ver(K):
-    while True:
-        K = input('What is the time format (12hr or 24hr)? \n ').lower()
-        if K != '12hr' and K != '24hr':
-            print('Invalid input. Please enter either 12hr or 24hr.')
-            continue
-        break
-    
-    return K
-def n_verifier(n):
-    while True:
-        try:
-            n = int(input('What is the number of participants in the group at the time the data was downloaded?\n'))
-            if n <= 1:
-                print('Invalid input. Please enter a positive integer.')
-                continue
-            break
-        except ValueError:
-            print('Invalid input. Please enter a valid positive integer.')
-    
-    return n
-def df_processing(raw_df):
+def df_processing(raw_df , n):
     df_notification = (raw_df[raw_df['user']== 'group_notification']).copy()
     df_notification.reset_index(inplace=True)
     df_notification.drop(columns='index', inplace=True)
     df_notification['joined_by_link'] = (df_notification['message'].str.contains('joined')).astype(int)
     df_notification['added'] = (df_notification['message'].str.contains('added')).astype(int)
     df_notification['left'] = (df_notification['message'].str.contains('left')).astype(int)
-    df_notification['n']= N
+    df_notification['n']= n
     df_notification['left_count']= 0
 
     for i in reversed(range(1,df_notification.shape[0])):
@@ -114,9 +99,6 @@ def df_processing(raw_df):
     day_of_creation = df_notification['date_time'].min()
     df_day1 = df['date_time'].min()
     df_end_day = df['date_time'].max()
-    print('The group was established on {}, The database begins in {} and ends in {}'.format(day_of_creation.date(),
-                                                                                          df_day1.date(),
-                                                                                          df_end_day.date()))
     df_notification = df_notification[df_notification['date_time']> day_of_creation]
     df.reset_index(inplace=True)
     df.drop(columns='index', inplace=True)
@@ -141,16 +123,17 @@ def group_by_day(df, df_notification):
     date_group =pd.concat([date_n_group,date_group,date_al_group],axis=1)
     date_group["engagement"]=date_group["message_c"]/date_group["n"]
     return date_group
-def plot_data():
+def plot_data(df ,date_group, raw_df, day_of_creation):
     col = sns.color_palette('Dark2')
     hour = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,0,1,2,3,4,5]
     days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    j_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     
     def top_10_active_days():
         sns.set_style('whitegrid')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         top10days = date_group.sort_values(by='message_c', ascending=False).head(10)
         sns.barplot(data=top10days, x=top10days.index.date, y='message_c', color = col[0])
         plt.title('Top 10 most active days')
@@ -175,7 +158,7 @@ def plot_data():
     def top_10_media_sharing():
         sns.set_style('whitegrid')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         top10media = df.groupby('user')['media_message'].sum().sort_values(ascending=False).head(10).reset_index()
         plt.bar(top10media['user'], top10media['media_message'], color = col[0])
         plt.xticks(rotation=15)
@@ -187,7 +170,7 @@ def plot_data():
     def active_hours():
         sns.set_style('whitegrid')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         grouped_by_time = df.groupby('hour').sum(numeric_only=True).reset_index().sort_values(by = 'hour')
         sns.barplot(x =grouped_by_time['hour'],y= grouped_by_time['message_c'] ,order=hour , color = col[0])
         plt.title('Most Active Hours')
@@ -200,7 +183,7 @@ def plot_data():
     def day_of_week():
         sns.set_style('whitegrid')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         grouped_by_day = df[['day_of_week', 'message_c']].groupby('day_of_week').sum(numeric_only=True).reset_index()
         sns.barplot(x=grouped_by_day['day_of_week'], y= grouped_by_day['message_c'], order=days, color=col[0])
         plt.ylabel('Message Count')
@@ -212,7 +195,7 @@ def plot_data():
     def month_group():
         sns.set_style('whitegrid')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         grouped_by_month = df[['month', 'message_c']].groupby('month').sum(numeric_only=True).reset_index()
         sns.barplot(x=grouped_by_month['message_c'], y = grouped_by_month['month'] , order = months, color=col[0])
         plt.ylabel('')
@@ -225,7 +208,7 @@ def plot_data():
     def dh_heatmap():
         sns.set_style('white')
         plt.fontsize = 10
-        plt.figure(figsize = (10 , 4.4))
+        plt.figure(figsize = (13,6))
         group_by_day_and_hour = df.groupby(['day_of_week', 'hour']).sum(numeric_only=True).reset_index()
         group_by_day_and_hour.drop(columns='year', inplace=True)
         pt = group_by_day_and_hour.pivot_table(index = 'hour', columns = 'day_of_week', values = 'message_c').reindex(index = hour, columns = days)
@@ -295,7 +278,7 @@ def plot_data():
     def leav_traffic():
         sns.set_style('white')
         plt.fontsize = 20
-        fig, ax1 = plt.subplots(figsize = (16, 4))
+        fig, ax1 = plt.subplots(figsize = (18, 6))
         ax1.plot(date_group.index,date_group['message_c'], color= col[2] , alpha=0.6 ,label='Daily Messages')
         ax2 = ax1.twinx()
         ax2.bar(date_group.index, height = date_group['left'] ,width = 1.5 , alpha=1 ,label='left Count', color=col[1])
@@ -360,48 +343,99 @@ def plot_data():
     pdf.cell(pdf.w - 10, 5, 'This analysis was created using DaChat WhatsApp group chat analyzer.  More information can be found here', link=github)
 
 
-    pdf.output('results/{}_pdf.pdf'.format(os.path.basename(file).split(".", 1)[0]))
+    pdf.output('results/{}_pdf.pdf'.format(df["group"][0]))
 
-    print('Plots were saved in the "plots" folder \nPDF report was saved in the "results" folder')
+class App():
+    def __init__(self):
+        def open_text_file():
+                # Specify the file types
+            filetypes = (('text files', '*.txt'), ('All files', '*.*'))
+            # Show the open file dialog by specifying the initial directory
+            file_path = fd.askopenfilename(filetypes=filetypes, initialdir="D:/Downloads")
+            if file_path:
+                file_text.delete('1.0', tk.END)
+                file_text.insert('1.0', file_path)
+        def run_analysis():
+            file = file_text.get('1.0', tk.END).rstrip()
+            N = int(num_choice.get())
+            category = str(category_choice.get())
+            if file and N and category:
+                raw_df = rawtodf(file, category)
+                day_of_creation = None
+                df , df_notification, day_of_creation = df_processing(raw_df, N)
+                if os.path.isfile('results/fdf.csv')and os.path.isfile('fdf_notif.csv'):
+                    df.to_csv('results/fdf.csv', mode='a', header=False, index=False)
+                    df.to_csv('results/fdf_notif.csv', mode='a', header=False, index=False)
+                else:
+                    df.to_csv('results/fdf.csv', index=False)
+                    df_notification.to_csv('results/fdf_notif.csv', index=False)
+                date_group = None
+                date_group = group_by_day(df , df_notification)
+                plot_data(df ,date_group, raw_df, day_of_creation)
+                tk.messagebox.showinfo(title='Congratulations!', message='Plots were saved in the "plots" folder \nPDF report was saved in the "results" folder \nThank you for using the DaChat')
+            else:
+                tk.messagebox.showwarning(title='Invalid Input', message='The user must enter all variables before running the analysis')
 
+        self.root = tk.Tk()
+        self.root.title('DaChat WhatsApp Analyzer')
+        self.root.iconphoto(False, tk.PhotoImage(file='images/s_logo.png'))
+        boldStyle = ttk.Style ()
+        boldStyle.configure('Bold.TButton', font = ('Helvetica','14','bold'))
+        
+        #Frames Arrangement
+        frame = tk.Frame(self.root)
+        frame.pack()
+
+        # Intro Frame
+        intro_frame = tk.LabelFrame(frame, text='Intro')
+        intro_frame.grid(row=0, column=0, sticky='news', padx=20, pady=10)
+
+        intro_0_label = tk.Label(intro_frame, justify='left' ,text='Export the WhatsApp chat you like to analyze using the following steps: \n1. Open the WhatsApp chat \n2.Click the three vertical dots on top-right \n3. Click More \n4. Click Export Chat \n5. Click Without Media \n6. Save the generated .txt file where it is accessible ')
+        intro_0_label.pack(padx=20, pady=10)
+
+        # File_Frame
+        file_frame = tk.LabelFrame(frame, text='Import Chat Data File')
+        file_frame.grid(row=1, column=0, sticky='news', padx=20, pady=10)
+
+        file_text = tk.Text(file_frame, height=1, width=40 , wrap="none")
+        file_text.grid(row=0, column=0, padx=10 ,pady=5)
+
+        file_button = ttk.Button(file_frame, text='Open File', command=open_text_file)
+        file_button.grid(row=0, column=1, padx=10 ,pady=5)
+
+
+        # Data Info Frame Arrangement
+        data_info_frame = tk.LabelFrame(frame,text='Chat Data info')
+        data_info_frame.grid(row=2, column=0, sticky='news', padx=20, pady=10)
+
+        #top label
+        data_info_label = tk.LabelFrame(data_info_frame, text='enter detels:')
+        data_info_label.grid(row=0, column=0, padx=10 ,pady=5)
+
+        #Data info labels
+
+        num_label = tk.Label(data_info_frame, text='number of \n participants in group')
+        num_label.grid(row=1 ,column=0, padx=10 ,pady=5)
+
+        category_label=tk.Label(data_info_frame, text='group category')
+        category_label.grid(row=1 ,column=1, padx=10 ,pady=5)
+
+        #Data info choice
+        num_choice = tk.Spinbox(data_info_frame, from_=0, to='infinity')
+        num_choice.grid(row=2 , column=0, padx=10 ,pady=5)
+
+        category_choice = tk.Entry(data_info_frame)
+        category_choice.grid(row=2, column=1, padx=10 ,pady=5)
+        
+        # Save to Button
+
+
+        # Submit Button Frame
+        submit_button = ttk.Button(frame, text='Run Analysis', style='Bold.TButton', command=run_analysis)
+        submit_button.grid(row=4 , column=0 ,sticky='news', padx=20, pady=20)
+
+        self.root.mainloop()
+        return
 
 if __name__ == '__main__':
-    print('After Exported the chat from your WhatsApp to your computer, this software will convert it into two processed CSV files')
-    
-    # Receiving a file from the user, checking the existence of the file and whether it is raw information from WhatsApp
-    #file = None
-    file = None
-    file = file_verifier(file)
-    # Receiving the hours format in file from the user
-    key = None
-    key = hour_formt_ver(key)
-    
-    # Receiving information about the group's neighborhood for my analysis needs
-    category = str(input('What is the group category? \n'))
-
-    # Knowing the number of users at the moment the information is downloaded allows reverse engineering of the numbers of participants in the group
-    # validating the software is receives an integer
-    N = None
-    N = n_verifier(N)
-
-    # Converts raw .txt file into a Data Frame
-    raw_df = rawtodf(file, key,category)
-    
-    # Dividing the raw information into user messages and system messages for further analysis
-    day_of_creation = None
-    df , df_notification, day_of_creation = df_processing(raw_df)
-
-    # Saveing processed data frame to device
-    if os.path.isfile('results/fdf.csv')and os.path.isfile('fdf_notif.csv'):
-        df.to_csv('results/fdf.csv', mode='a', header=False, index=False)
-        df.to_csv('results/fdf_notif.csv', mode='a', header=False, index=False)
-        print('Processed Data Frames have been added to the files no your computer')
-    else:
-        df.to_csv('results/fdf.csv', index=False)
-        df_notification.to_csv('results/fdf_notif.csv', index=False)
-        print('Processed csv files have been saved to your computer')
-    
-    date_group = None
-    date_group = group_by_day(df , df_notification)
-    plot_data()
-    print('Thank you for using the DaChat \nGoodbye')
+    App()
